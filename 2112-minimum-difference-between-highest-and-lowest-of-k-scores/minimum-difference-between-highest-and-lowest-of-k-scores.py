33dd4e8da7c4ed99ad36ee13d100302200bd3c0b
SiddML101 <158842608+SiddML101@users.noreply.github.com>
2026-01-25 17:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.85%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -10,13 +10,16 @@ class Solution:
         nums.sort()
         min_n = 0
         max_n = 0
-        for i in range (len(nums)-k+1):
-            for j in range (i, k+i):
-                max_n = nums[j]
+        j = k-1
+        i = 0
+        while j < len(nums):
+            max_n = nums[j]
             min_n = nums[i]
             diff = max_n - min_n
             if diff < min_diff:
                 min_diff = diff
+            i += 1
+            j += 1
             
         return min_diff
                 
