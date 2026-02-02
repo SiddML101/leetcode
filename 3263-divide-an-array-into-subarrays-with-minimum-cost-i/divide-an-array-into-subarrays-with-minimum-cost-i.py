1f72edc7d5e0389c396c724ad28135337491c385
SiddML101 <158842608+SiddML101@users.noreply.github.com>
2026-02-02 17:20:49 +0530

    Time: 3 ms (36.27%) | Memory: 19.3 MB (50.26%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -3,16 +3,17 @@ class Solution:
         if len(nums) == 3:
             return sum(nums)
         i,j = 1,2
+        a = nums[i]
         min_sum = float('inf')
-        while i < len(nums)-1:
-            j = i+1
-            r_sum = 0
-            while j < len(nums):
-                r_sum = nums[0]+ nums[i] + nums[j]
-                if r_sum < min_sum:
-                    min_sum = r_sum
-                j +=1
-            i+= 1
+        while j < len(nums):
+            r_sum = nums[0] + a + nums[j]
+            if r_sum < min_sum:
+                min_sum = r_sum
+            j += 1
+            i += 1
+            if nums[i] < a:
+                a = nums[i]
+
         return min_sum
 
 
