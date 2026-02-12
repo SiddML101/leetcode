d1b183556548a6b1d08fe4c785ff14df3557b8f7
SiddML101 <158842608+SiddML101@users.noreply.github.com>
2026-02-12 12:01:53 +0530

    Time: 1376 ms (89.00%) | Memory: 19.2 MB (62.89%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
@@ -1,22 +1,25 @@
 class Solution:
     def longestBalanced(self, s: str) -> int:
         max_count = 0
-        for i in range (len(s)):
+
+        for i in range(len(s)):
             hash_map = {}
-            count = 0
-            for j in range (i,len(s)):
+            max_freq = 0
+
+            for j in range(i, len(s)):
                 if s[j] in hash_map:
-                    hash_map[s[j]] +=  1
+                    hash_map[s[j]] += 1
                 else:
                     hash_map[s[j]] = 1
 
-                a = max(hash_map.values())
-                all_max = all(value == a for value in hash_map.values())
-                if all_max is True:
-                    count = j - i + 1
-                    if max_count < count:
-                        max_count = count
+                if hash_map[s[j]] > max_freq:
+                    max_freq = hash_map[s[j]]
+
+                distinct = len(hash_map)
+                length = j - i + 1
+
+                if max_freq * distinct == length:
+                    if max_count < length:
+                        max_count = length
+
         return max_count
-
-
-
