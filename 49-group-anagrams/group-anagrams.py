ed7d765ced5d0f5c6e7d873159888ec05ac33b6c
SiddML101 <158842608+SiddML101@users.noreply.github.com>
2026-09-20 12:49:53 +0530

    Time: 11 ms (82.71%) | Memory: 22 MB (65.03%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        hashmap = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(word)
+
+        return list(hashmap.values())
+
+
+
+
+
+
+
+
+        
+
+        
